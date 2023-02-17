1972ff736c1486785bc442ee6e50da220701e0d9
devikapramesti <devikapramesti4@gmail.com>
2023-02-17 17:14:49 +0700

    Chapter 2

--- src/components/album-galery.jsx
+++ src/components/album-galery.jsx
@@ -52,7 +52,7 @@ const products = [
     return (
       <div className="bg-white">
         <div className="mx-auto max-w-2xl py-16 px-4 sm:py-24 sm:px-6 lg:max-w-7xl lg:px-8">
-          <h2 className="text-2xl text-bold">Collection</h2>
+          <h2 className="text-2xl text-bold"><Collection></Collection></h2>
   
           <div className="grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
             {products.map((product) => (
